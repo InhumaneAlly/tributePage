c2cbedcfe7e61c8821f0eec0efc865f11a823da2
Deans Antony <inhumaneally@gmail.com>
2020-09-03 22:36:50 +0530

    content-pending

---
+++ JS/myjs.js
@@ -0,0 +1,97 @@
+
+var termNumb = document.querySelector(".termNumber").innerHTML;
+var termNumb1 = document.querySelector(".termNumber1").innerHTML;
+var termNumb2 = document.querySelector(".termNumber2").innerHTML;
+
+
+setInterval(function () {
+    termNumb = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber").innerHTML = termNumb;
+}, 100);
+setInterval(function () {
+    termNumb1 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber1").innerHTML = termNumb1;
+}, 805);
+setInterval(function () {
+    termNumb2 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber2").innerHTML = termNumb;
+}, 90);
+setInterval(function () {
+    termNumb3 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber3").innerHTML = termNumb3;
+}, 150);
+setInterval(function () {
+    termNumb4 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber4").innerHTML = termNumb4;
+}, 125);
+setInterval(function () {
+    termNumb5 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber5").innerHTML = termNumb5;
+}, 102);
+setInterval(function () {
+    termNumb6 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber6").innerHTML = termNumb6;
+}, 135);
+setInterval(function () {
+    termNumb7 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber7").innerHTML = termNumb7;
+}, 620);
+setInterval(function () {
+    termNumb8 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber8").innerHTML = termNumb8;
+}, 345);
+setInterval(function () {
+    termNumb9 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber9").innerHTML = termNumb9;
+}, 160);
+setInterval(function () {
+    termNumb9 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber9").innerHTML = termNumb9;
+}, 300);
+setInterval(function () {
+    termNumb10 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber10").innerHTML = termNumb10;
+}, 250);
+setInterval(function () {
+    termNumb11 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber11").innerHTML = termNumb11;
+}, 700);
+setInterval(function () {
+    termNumb12 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber12").innerHTML = termNumb12;
+}, 100);
+setInterval(function () {
+    termNumb13 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber13").innerHTML = termNumb13;
+}, 800);
+setInterval(function () {
+    termNumb14 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber14").innerHTML = termNumb14;
+}, 490);
+setInterval(function () {
+    termNumb15 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber15").innerHTML = termNumb15;
+}, 690);
+setInterval(function () {
+    termNumb16 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber16").innerHTML = termNumb16;
+}, 1000);
+setInterval(function () {
+    termNumb17 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber17").innerHTML = termNumb17;
+}, 420);
+setInterval(function () {
+    termNumb18 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber18").innerHTML = termNumb18;
+}, 670);
+setInterval(function () {
+    termNumb19 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber19").innerHTML = termNumb19;
+}, 90);
+setInterval(function () {
+    termNumb20 = termNumb[termNumb.length - 1] + termNumb.substring(0, termNumb.length - 1);
+    document.querySelector(".termNumber20").innerHTML = termNumb20;
+}, 10);
+
+
+
